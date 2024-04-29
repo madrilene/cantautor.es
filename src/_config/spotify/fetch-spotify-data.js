1df97fb39c8bda72e5be858bc4b7aeca5635c9ed
madrilene <hola@lenesaile.com>
2024-04-29 09:19:40 +0200

    implement try/ catch

--- src/_config/spotify/fetch-spotify-data.js
+++ src/_config/spotify/fetch-spotify-data.js
@@ -13,73 +13,77 @@ const PLAYLIST_ENDPOINT = `https://api.spotify.com/v1/playlists/${PLAYLIST}`;
 const TOKEN_ENDPOINT = 'https://accounts.spotify.com/api/token';
 
 async function fetchSpotifyData() {
-  const auth = Buffer.from(`${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`).toString(
-    'base64'
-  );
-  const accessTokenResponse = await fetch(TOKEN_ENDPOINT, {
-    method: 'POST',
-    headers: {
-      'Authorization': `Basic ${auth}`,
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    body: new URLSearchParams({
-      grant_type: 'client_credentials'
-    })
-  });
-  const accessToken = await accessTokenResponse.json().then(data => data.access_token);
+  try {
+    const auth = Buffer.from(
+      `${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`
+    ).toString('base64');
+    const accessTokenResponse = await fetch(TOKEN_ENDPOINT, {
+      method: 'POST',
+      headers: {
+        'Authorization': `Basic ${auth}`,
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: new URLSearchParams({
+        grant_type: 'client_credentials'
+      })
+    });
+    const accessToken = await accessTokenResponse.json().then(data => data.access_token);
 
-  const playlistResponse = await fetch(PLAYLIST_ENDPOINT, {
-    headers: {
-      Authorization: `Bearer ${accessToken}`
+    const playlistResponse = await fetch(PLAYLIST_ENDPOINT, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
+    });
+    const playlist = await playlistResponse.json();
+
+    let artists = [];
+
+    for (let track of playlist.tracks.items) {
+      for (let artist of track.track.artists) {
+        const artistDetailsResponse = await fetch(`https://api.spotify.com/v1/artists/${artist.id}`, {
+          headers: {Authorization: `Bearer ${accessToken}`}
+        });
+        const artistDetails = await artistDetailsResponse.json();
+
+        const slug = slugify(artist.name, {lower: true});
+        const artistDir = `./src/assets/artists/${slug}`;
+        ensureDirectoryExists(artistDir);
+
+        const profileImagePath = `${artistDir}/profile.jpg`;
+        if (artistDetails.images && artistDetails.images.length > 0) {
+          const imageUrl = artistDetails.images[0].url;
+          await downloadFile(imageUrl, profileImagePath, true); // true to force download
+        }
+
+        const albumImagePath = `${artistDir}/album.jpg`;
+        if (track.track.album && track.track.album.images && track.track.album.images.length > 0) {
+          const albumImageUrl = track.track.album.images[0].url;
+          await downloadFile(albumImageUrl, albumImagePath, true); // true to force download
+        }
+
+        const previewMp3Path = `${artistDir}/preview.mp3`;
+        if (track.track.preview_url) {
+          await downloadFile(track.track.preview_url, previewMp3Path, true); // true to force download
+        }
+
+        artists.push({
+          name: artist.name,
+          trackName: track.track.name,
+          profileImage: profileImagePath.replace('./src', ''),
+          albumImage: `/assets/artists/${slug}/album.jpg`,
+          albumName: track.track.album ? track.track.album.name : 'Unknown Album',
+          preview: `/assets/artists/${slug}/preview.mp3`,
+          spotifyUrl: artist.external_urls.spotify
+        });
+      }
     }
-  });
-  const playlist = await playlistResponse.json();
 
-  let artists = [];
-
-  for (let track of playlist.tracks.items) {
-    for (let artist of track.track.artists) {
-      const artistDetailsResponse = await fetch(`https://api.spotify.com/v1/artists/${artist.id}`, {
-        headers: {Authorization: `Bearer ${accessToken}`}
-      });
-      const artistDetails = await artistDetailsResponse.json();
-
-      const slug = slugify(artist.name, {lower: true});
-      const artistDir = `./src/assets/artists/${slug}`;
-      ensureDirectoryExists(artistDir);
-
-      const profileImagePath = `${artistDir}/profile.jpg`;
-      if (artistDetails.images && artistDetails.images.length > 0) {
-        const imageUrl = artistDetails.images[0].url;
-        await downloadFile(imageUrl, profileImagePath, true); // true to force download
-      }
-
-      const albumImagePath = `${artistDir}/album.jpg`;
-      if (track.track.album && track.track.album.images && track.track.album.images.length > 0) {
-        const albumImageUrl = track.track.album.images[0].url;
-        await downloadFile(albumImageUrl, albumImagePath, true); // true to force download
-      }
-
-      const previewMp3Path = `${artistDir}/preview.mp3`;
-      if (track.track.preview_url) {
-        await downloadFile(track.track.preview_url, previewMp3Path, true); // true to force download
-      }
-
-      artists.push({
-        name: artist.name,
-        trackName: track.track.name,
-        profileImage: profileImagePath.replace('./src', ''),
-        albumImage: `/assets/artists/${slug}/album.jpg`,
-        albumName: track.track.album ? track.track.album.name : 'Unknown Album',
-        preview: `/assets/artists/${slug}/preview.mp3`,
-        spotifyUrl: artist.external_urls.spotify
-      });
-    }
+    // Save to a local JSON file for caching and easier access during development
+    console.log('Spotify data updated successfully.');
+    fs.writeFileSync('./src/_data/artists-generated.json', JSON.stringify(artists, null, 2));
+  } catch (error) {
+    console.error('An error occurred:', error);
   }
-
-  // Save to a local JSON file for caching and easier access during development
-  console.log('Spotify data updated successfully.');
-  fs.writeFileSync('./src/_data/artists-generated.json', JSON.stringify(artists, null, 2));
 }
 
 fetchSpotifyData();
